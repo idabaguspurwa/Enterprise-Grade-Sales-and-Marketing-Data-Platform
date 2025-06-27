c94e6f6cf8b81832f16e72ce44255b32d1a70104
idabaguspurwa <ida.bagus862@gmail.com>
2025-06-27 18:45:39 +0700

    fixing fct_orders.sql code

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -15,15 +15,14 @@ SELECT
     order_items.order_item_id,
     order_items.order_id,
     order_items.product_id,
-
     orders.user_id AS customer_id,
 
-    -- Product details from the products table
+    -- Product details
     products.name AS product_name,
     products.category AS product_category,
     products.department AS product_department,
 
-    -- Order details from the orders table
+    -- Order details
     orders.order_status,
     orders.order_created_at,
     
@@ -32,8 +31,7 @@ SELECT
     products.cost AS item_cost,
     (order_items.sale_price - products.cost) AS item_profit,
 
-    -- Timestamps
-    TIMESTAMP_DIFF(orders.shipped_at, orders.created_at, HOUR) AS hours_to_ship
+    TIMESTAMP_DIFF(orders.shipped_at, orders.order_created_at, HOUR) AS hours_to_ship
 
 FROM order_items
 LEFT JOIN orders ON order_items.order_id = orders.order_id
